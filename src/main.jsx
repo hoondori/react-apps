c20829288d6372d0d16a8064097c2b65feedfb90
hoondori <hoondori@gmail.com>
2025-01-26 08:16:01 +0900

    firebase 연동

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
 import App from './App.jsx'
+import firebaseApp from './firebase.jsx'
+console.log(firebaseApp);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
