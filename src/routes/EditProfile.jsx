dec22f2be777d1009f5fd919d521c2542c70375a
hoondori <hoondori@gmail.com>
2025-01-28 19:47:37 +0900

    내 트윗만 firebase storage에서 필터링해서 가죵호기 EditProfile 에서 목록 보여주기

--- src/routes/EditProfile.jsx
+++ src/routes/EditProfile.jsx
@@ -1,17 +1,48 @@
-import { authService } from "@/fbase.jsx"
+import { authService, dbService } from "@/fbase.jsx"
 import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
+import Nweet from "@components/Nweet";
 
-const EditProfile = () => {
+const EditProfile = ({ userObj }) => {
     const navigate = useNavigate();
+    const [nweets, setNweets] = useState([]); // 트윗 목록
 
     const onLogoutClick = () => {
         authService.signOut();
         navigate("/");  // 로그 아웃 후 Home으로 이동
     }
 
+    // 내 트윗 보여주기
+    const getMyNweets = async () => {
+        const nweets = await dbService
+            .collection("nweets")
+            .where("creatorId", "==", userObj.uid)
+            .orderBy("createdAt", "asc")
+            .get();
+
+        const newArray = nweets.docs.map((document)=>({
+            id: document.id, ...document.data(),
+        }));
+        setNweets(newArray);
+
+        console.log(nweets.docs.map((doc)=> doc.data()));
+    };
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
+
     return (
         <>
             <button onClick={onLogoutClick}>Log Out</button>
+            <div>
+                {nweets.map((nweet) => (
+                    <Nweet 
+                        key={nweet.id} 
+                        nweetObj={nweet} 
+                        isOwner={true} />
+                ))}
+            </div>            
         </>
     );
 };
