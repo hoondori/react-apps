27d1b7d0098980added83dc3141f96f4eda72090
hoondori <hoondori@gmail.com>
2025-03-22 13:18:02 +0900

    portpolois update

---
+++ portfolio-clone/src/components/templates/AboutMeSection.tsx
@@ -0,0 +1,100 @@
+/**  @jsxImportSource @emotion/react */
+
+import { Center } from '@components/atoms/Center'
+import { Text } from '@components/atoms/Text'
+import { Section } from '@components/organisms/Section'
+import { motion } from 'framer-motion'
+import { useTheme } from '@emotion/react'
+import { forwardRef, ForwardRefRenderFunction } from 'react'
+import { SectionTitle } from '@components/organisms/SectionTitle'
+import { SectionMethodsType } from 'pages'
+
+
+// personal information
+const ABOUT_ME_FIELD = [
+  { title: '이름', fields: ['정성훈'] },
+  { title: '생년월일', fields: ['1978.09.19'] },
+  { title: '연락처', fields: ['010-8455-5285', 'hoondori@gmail.com'] },
+]
+
+// define props
+type AboutMeSectionProps = {
+  sectionMethods: SectionMethodsType
+}
+
+const AboutMeSection: ForwardRefRenderFunction<
+  HTMLDivElement,
+  AboutMeSectionProps
+> = ({ sectionMethods}, ref) => {
+  const theme = useTheme()
+
+  return (
+    <Section
+      ref={ref}
+      css={{
+
+      }}
+    >
+      <Center
+        css={{
+
+        }}
+      >
+        <SectionTitle
+          title='ABOUT ME' 
+          onClickScrollClip={()=>sectionMethods.scrollToSection(1)}  
+        />
+        // show personal information with motion
+        <motion.div
+          initial={{ translateY: 0, opacity: 0 }}
+          whileInView={{
+            translateY: -20,
+            opacity: 1,
+          }}
+          transition={{
+            delay: 0.6,
+            x: { duration: 1 },
+            default: { ease: 'linear' },
+          }}
+          viewport={{
+            once: true,
+          }}
+          css={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'space-around',
+            marginTop: '4rem',
+            gap: '3rem',
+            flexDirection: 'column',
+            '@media (min-width: 800px)': {
+              flexDirection: 'row',
+            },
+          }}          
+        >  
+          {ABOUT_ME_FIELD.map( ({fields, title})=> (
+            <div 
+              key={title}
+              css={{}}
+            >
+              <Text
+                css={{}}
+              >
+                {title}
+              </Text>
+              {fields.map((field)=>(
+                <Text
+                  key={field}
+                  css={{}}
+                >
+                  {field}
+                </Text>
+              ))}
+            </div>
+          ))}
+        </motion.div>
+      </Center>
+    </Section>
+  )
+}
+
+export default forwardRef(AboutMeSection)
